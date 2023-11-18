7a695c168593641199fd4ad7193596a8d631a526
Nathaniel81 <nathanield356@gmail.com>
2023-11-18 13:36:53 +0300

    Update base template

--- core/urls.py
+++ core/urls.py
@@ -1,6 +1,4 @@
 from django.urls import path
-from django.conf import settings
-from django.conf.urls.static import static
 from . import views
 
 
@@ -9,4 +7,4 @@ urlpatterns = [
 	path('booking/<str:code>', views.booking, name='booking'),
 	path('scheduled', views.scheduled, name='scheduled'),
 	path('delete_booking/<str:code>', views.delete_booking, name='delete-booking')
-]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
