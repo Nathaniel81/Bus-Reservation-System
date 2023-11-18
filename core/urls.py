6910e1498d7415dff59d3abe2fded4702e1dfd2d
Nathaniel81 <nathanield356@gmail.com>
2023-11-18 09:04:44 +0300

    configure static and media files dirs

--- core/urls.py
+++ core/urls.py
@@ -1,4 +1,6 @@
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
 from . import views
 
 
@@ -7,4 +9,4 @@ urlpatterns = [
 	path('booking/<str:code>', views.booking, name='booking'),
 	path('scheduled', views.scheduled, name='scheduled'),
 	path('delete_booking/<str:code>', views.delete_booking, name='delete-booking')
-]
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
