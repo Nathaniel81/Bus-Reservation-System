0dca7b9af9e95379abf3ca116c8d783a4633dbda
Nathaniel81 <nathanield356@gmail.com>
2023-11-19 10:00:22 +0300

    Integrate dynamic data binding with the search results template

--- static/js/script.js
+++ static/js/script.js
@@ -1,4 +1,23 @@
 document.addEventListener('DOMContentLoaded', function () {
-	var today = new Date().toISOString().split('T')[0];
-	document.getElementById('date').min = today;
+	var date = document.getElementById('date');
+	if (date){
+		var today = new Date().toISOString().split('T')[0];
+		date.min = today;
+	}
+
+	function highlightActiveLink() {
+		var path = window.location.pathname;
+		document.querySelectorAll('.navbar-nav .nav-item .nav-link').forEach(function (link) {
+		  var href = link.getAttribute('href');
+		  if (path === href) {
+			link.classList.add('active');
+		  } else {
+			link.classList.remove('active');
+		  }
+		});
+	  }
+
+	  window.onload = highlightActiveLink;
+	  window.addEventListener('hashchange', highlightActiveLink);
+	  window.addEventListener('popstate', highlightActiveLink);
 });
