41056873eba03beff8f0403e94e0f5d238a92b70
Nathaniel81 <nathanield356@gmail.com>
2023-11-20 19:11:32 +0300

    Enhance login form handling with AJAX

--- authentication/forms.py
+++ authentication/forms.py
@@ -1,7 +1,5 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
-from crispy_forms.helper import FormHelper
-from crispy_forms.layout import Layout
 from core.models import User
 
 class LoginForm(forms.Form):
