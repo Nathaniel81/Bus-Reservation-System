4a9d2da151e81667cc9a9fe3028a8a5a26ae7a50
Nathaniel81 <nathanield356@gmail.com>
2023-11-20 23:54:47 +0300

    Enhance login modal with a spinner to indicate loading during authentication

--- authentication/templatetags/custom_tags.py
+++ authentication/templatetags/custom_tags.py
@@ -5,4 +5,8 @@ register = template.Library()
 
 @register.inclusion_tag('authentication/login_form_modal.html', takes_context=True)
 def login_form_modal(context):
-    return {'login_form': LoginForm(), 'signup_form': SignUpForm()}
+    return {'login_form': LoginForm()}
+
+@register.inclusion_tag('authentication/signup_form_modal.html', takes_context=True)
+def signup_form_modal(context):
+    return {'signup_form': SignUpForm()}
